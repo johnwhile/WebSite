04931e5311b6481dca79658e78c1163b78246cc7
HPnotebook <johnwhilemail@gmail.com>
2025-08-19 17:19:54 +0200

    19082025

--- js/functions.js
+++ js/functions.js
@@ -2,5 +2,11 @@ let fileHandle;
 
 async function SaveFile()
 {
+    console.log("akdjasdasd");
     [fileHandle] = await window.showOpenFilePicker();
+}
+
+function myFunction() 
+{
+  alert("I am an alert box!");
 }
