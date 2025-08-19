fb262ecf6864f1e90b25138c99099e8a8d1fc6de
HPnotebook <johnwhilemail@gmail.com>
2025-08-19 17:49:42 +0200

    19082025

--- js/functions.js
+++ js/functions.js
@@ -2,11 +2,26 @@ let fileHandle;
 
 async function SaveFile()
 {
-    console.log("akdjasdasd");
-    [fileHandle] = await window.showOpenFilePicker();
+    console.log("Salvataggio Scontrino");
+
+    let name = document.getElementById("lotto").value;
+    console.log(name);
+    const opts = 
+    {
+    suggestedName : name,
+    types: [{
+      desciption: "Scontrino",
+      accept: { "text/plain": [".txt"] },
+    },],
+    };
+
+    [fileHandle] = await window.showSaveFilePicker(opts);
 }
 
-function myFunction() 
+function DebugFn() 
 {
-  alert("I am an alert box!");
+  let name = document.getElementById("lotto").value.toString();
+  console.log(typeof name);
+  console.log(name);
+  alert(name);
 }
