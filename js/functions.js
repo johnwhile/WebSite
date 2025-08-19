61461e809050376dacca4c51c9499979046f9b79
HPnotebook <johnwhilemail@gmail.com>
2025-08-19 18:26:07 +0200

    19082025

--- js/functions.js
+++ js/functions.js
@@ -26,15 +26,49 @@ function DebugFn()
   alert(name);
 }
 
+function format_counter(counter)
+{
+  return counter;
+  //string.Format("{0,7}", counter);
+} 
+
+function format_pad14(left,  right)
+{
+    if (left == undefined) return "";
+
+     if (right == undefined)
+     {
+        return  left.padEnd(14);
+     }
+     else return left.padEnd(14 - right.length) + right;
+}
+
+
 function SaveTextFile()
 {
-  var content = "asdasdad";
+  let operatore = document.getElementById("operatore").value;
+  let articolo = document.getElementById("articolo").value;
+  let lotto = document.getElementById("lotto").value;
+  let barattoli = Number.parseInt(document.getElementById("barattoli").value);
+
+
+  console.log("operatore: " + operatore);
+  console.log("articolo:" + articolo);
+  console.log("lotto: "+ lotto);
+  console.log("barattoli: " + barattoli);
+  
+
+  var content = "OPERATORE\n";
+  content += operatore;
+  content += "\n";
+  content += format_pad14("LINEA") + " " + format_counter(1);
+
 
 
   const link = document.createElement("a");
   const file = new Blob([content], { type: 'text/plain' });
   link.href = URL.createObjectURL(file);
-  link.download = document.getElementById("lotto").value + ".txt";
+  link.download = lotto + ".txt";
   link.click();
   URL.revokeObjectURL(link.href);
 }
